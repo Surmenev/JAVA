92811b04fe79b94dd32ba27fae7b3d6913b40cfb
Surmenev <surmenevsi@mail.ru>
2021-04-13 15:54:13 +0500

    Lesson 02

--- FirstClass.java
+++ FirstClass.java
@@ -1,47 +1,59 @@
-package ru.geekbrains.lesson01;
+package Lesson2;
 
 public class FirstClass {
     public static void main(String[] args) {
-        printThreeWords();
-        checkSumSign();
-        printColor();
-        compareNumbers();
+        summa();
+        zero();
+        truefalse();
+        onemore();
     }
 
-    public static void printThreeWords() {
-        System.out.println("Orange");
-        System.out.println("Banana");
-        System.out.println("Apple");
-        System.out.println(" ");}
-
-    public static void checkSumSign() {
-        int a = 2;
-        int b = -2;
-        if ((a + b) >= 0) {
-            System.out.println("Положительное");}
-        else {
-            System.out.println("Отрицательное"); }
-        System.out.println(" ");}
-
-    public static void printColor() {
-        int value = 101;
-               if (value <= 0) {
-            System.out.println("Красный");}
-        else if (value <= 100) {
-            System.out.println("Жёлтый");}
-            else {
-            System.out.println("Зелёный");}
+    // 001
+    public static void summa() {
+        int a = 5;
+        int b = 16;
+        if (((a + b) >= 10) && ((a + b) <= 20)) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
         }
-
-    public static void  compareNumbers() {
-        int a = 3;
-        int b = 2;
-        if (a >= b) {
-            System.out.println("a >= b");}
-        else {
-            System.out.println("a < b");}
-    }
+        System.out.println(" ");
     }
 
+    // 002
+    public static void zero() {
+        int a = -1;
 
+        if (a < 0) {
+            System.out.println("Отрицательное");
+        } else {
+            System.out.println("Положительное");
+        }
+        System.out.println(" ");
+    }
 
+    // 003
+    public static void truefalse() {
+        int a = 1;
+
+        if (a < 0) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+        System.out.println(" ");
+    }
+
+    // 004
+    public static void onemore() {
+        int a = 5;
+        String b = "Hello!";
+
+            while (a > 0){
+            System.out.println(b);
+            a--;
+        }
+        System.out.println(" ");
+
+    }
+}
